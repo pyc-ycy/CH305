eedcc60b057f266fbb20874b8a614cb7212ea962
御承扬 <2923616405@qq.com>
2019-10-03 22:57:19 +0800

    Update Files

---
+++ content.h
@@ -0,0 +1,30 @@
+#ifndef CONTENT_H
+#define CONTENT_H
+
+#include <QFrame>
+#include <QStackedWidget>
+#include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include "baseifon.h"
+#include "contact.h"
+#include "detail.h"
+
+
+class Content : public QFrame
+{
+    Q_OBJECT
+
+public:
+    Content(QWidget *parent = 0);
+    ~Content();
+    QStackedWidget *stack;
+    QPushButton *AmendBtn;
+    QPushButton *CloseBtn;
+    BaseIfon *baseInfo;
+    Contact *contact;
+    Detail *detail;
+
+};
+
+#endif // CONTENT_H
